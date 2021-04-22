14a72bf30a2e7489a2b929f30aa92f5dd16298e6
nekita13 <nekitagundorov87006@gmail.com>
2021-04-22 18:08:56 +0400

    end

---
+++ inputD.py
@@ -0,0 +1,3 @@
+question  =["сколько лет сталину" , "калм пидор? ", "3", "rjybw"]
+answer = [["+56", "92" , "12"],["нет+", "да" , "он женщина"],["3localhost", "setting3" , "fiaop"],["localhost4", "4setting" , "fiaop"]]
+dif = [1,1,2,2]
--- main.py
+++ main.py
@@ -6,68 +6,77 @@ import inputD
 time.sleep(2)
 os.system('cls' if os.name == 'nt' else 'clear')
 
-userI = input("""выберите сложность
-         1 lite
-         2 midle
-         3 herd """)
-
-def main():
+score = 0
+win = 0
+o = 0
+def main(dif):
+    global win
+    global o 
+    global score
     id =0
+    dif = int(dif)
     for i in inputD.dif:
-        id +=1 
-        if i == 1 :
+        
+        if i == dif :
+            o+=1
             print(inputD.question[id])
-
-
-def what(dif):
-    if dif == 1:
-        v = int(random.uniform(1,99))
-        end = int(random.uniform(1,359))
-        print("x + "+str(v)+" = "+str(end))
-        while 1 :
-            samp = int(random.uniform(1 ,3))
-            print(""" варианты ответа""")
-            if samp == 1 : 
-                print("   1 +"+str(end) ) 
+            print("""введите ответ""")
+            swap = int(random.uniform(1 ,4))
+            if swap == 1 : 
+                print("   1 :"+inputD.answer[id][0]) 
+            elif swap == 2:
+                print("   1 :"+inputD.answer[id][1]) 
             else: 
-                print("   1 :"+str(end + int(random.uniform(1, 35)))) 
-            
-            if samp == 2 : 
-                print("   2 +"+str(end) ) 
+                print("   1 :"+inputD.answer[id][2]) 
+
+            if swap == 2 : 
+                print("   2 :"+ inputD.answer[id][0]) 
+            elif swap == 1: 
+                print("   2 :"+inputD.answer[id][2] )     
+            else : print("   2 :"+inputD.answer[id][1] ) 
+
+            if swap == 3 : 
+                print("   3 :"+inputD.answer[id][0] ) 
+            elif swap == 2 :
+                print("   3 :"+inputD.answer[id][2])
+            elif swap == 1: print("   3 :"+inputD.answer[id][1])
+            userI = input()
+            if userI == "сменить":
+                score -=dif *8
+                break
+            if userI == str(swap):
+                print("!!!молодец хорошая кися, ты победил!!!")   
+                score += dif * 10
+                win += 1 
             else: 
-                print("   2 "+str(end + int(random.uniform(1, 25))) ) 
-
-            if samp == 3 : 
-                print("   3 +"+str(end) ) 
-            else: print("   3 "+str(end + int(random.uniform(1, 50))) ) 
+                score -= dif * 10
+                print("ты обосрался")  
+            time.sleep(2)  
+            os.system('cls' if os.name == 'nt' else 'clear')
             
+
+        id +=1 
+
+
+
+
+while 1 :
+    score = 0
+    win = 0
+    userI = input("""выберите сложность
+            1 lite
+            2 midle
+            3 nerd 
             
-            userI = input("выберите ответ")
-            
-            if str(samp) == userI:
-                print("молодец хорошая кися, ты победил")
-                break
-            elif samp == 1 or samp == 2 or samp == 3:
-                print("ты проигрял")
-                break
-            else : print(" введи 1 или 2 или 3")
+    """)
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    if str(userI) ==str(1 )or str(userI) ==str(2) or str(userI) ==str(3):
+        main(userI)
+        print("you score = " + str(score))
+        print("ты ответил правильно на " + str(win) + "из" + str(o))
+    else : print(" ты рукажоп) введи ")
+    input("press enter to contine")
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 
-
-
-
-
-
-
-main()
-#for i in range(0, 4):
-#        
-#    if userI == "1" :
-#        print("ти пидор")
-#        what(1)
-#    elif userI == "2":
-#        print ("ну вроде не дурак")
-#    elif userI == "3": 
-#        print("теперь ты НАСТОЯЩИЙ МУЖИК")
-#    else : print("введите значение от 1 до 3")
-#
