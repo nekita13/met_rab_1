8aa67e1578a0e8cbebe1d3af00d5af9d8dd1ce6c
nekita13 <nekitagundorov87006@gmail.com>
2021-04-22 15:13:21 +0400

    wq

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ print("hello aldas")
 import os
 import random
 import time 
+import inputD
 time.sleep(2)
 os.system('cls' if os.name == 'nt' else 'clear')
 
@@ -9,6 +10,14 @@ userI = input("""выберите сложность
          1 lite
          2 midle
          3 herd """)
+
+def main():
+    id =0
+    for i in inputD.dif:
+        id +=1 
+        if i == 1 :
+            print(inputD.question[id])
+
 
 def what(dif):
     if dif == 1:
@@ -42,20 +51,23 @@ def what(dif):
                 print("ты проигрял")
                 break
             else : print(" введи 1 или 2 или 3")
-            
 
 
 
 
 
-for i in range(0, 4):
-        
-    if userI == "1" :
-        print("ти пидор")
-        what(1)
-    elif userI == "2":
-        print ("ну вроде не дурак")
-    elif userI == "3": 
-        print("теперь ты НАСТОЯЩИЙ МУЖИК")
-    else : print("введите значение от 1 до 3")
 
+
+
+main()
+#for i in range(0, 4):
+#        
+#    if userI == "1" :
+#        print("ти пидор")
+#        what(1)
+#    elif userI == "2":
+#        print ("ну вроде не дурак")
+#    elif userI == "3": 
+#        print("теперь ты НАСТОЯЩИЙ МУЖИК")
+#    else : print("введите значение от 1 до 3")
+#
